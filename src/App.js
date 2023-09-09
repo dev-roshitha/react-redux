5829520a29a3dceef02b78529e6252c73e519b00
unknown <sajithroshitha98@gmail.com>
2023-09-10 00:12:42 +0530

    customize react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/home"
+import About from "./pages/about"
+import Navbar from './components/nav-bar';
+import Footer from './components/footer';
+
+const Layout = () => {
+  return(<>
+    <div className="app">
+      <Navbar/>
+      <Footer/>
+    </div>
+  </>)
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/about',
+        element: <About/>
+      },
+    ]
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/nav-bar"
+
+const Home = () => {
+  return(
+    <div>
+      <Navbar/>
+      <h1>Home Page WITH REACT ROUTER DOM</h1>
+    </div>
+  )
+}
+
+export default Home
