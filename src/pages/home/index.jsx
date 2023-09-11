0dc5cb4fd928b308f95c70134b6c478d319e632a
unknown <sajithroshitha98@gmail.com>
2023-09-11 23:18:10 +0530

    update app

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,26 @@
+import { useReducer } from "react";
+
+const reducer = (state, action)=> {}
+
 const Home = () => {
+
+  const [state, setState] = useReducer(reducer, {
+    name: "roshitha",
+    email: "r@gmail.com",
+    address: "34/18 Kandy Srilanka",
+    phone: "0703639744"
+  })
+
   return(
     <div>
       <h1>Home Page WITH REACT ROUTER DOM</h1>
+      <div className="home-wrapper">
+        <input type="text" value={state.name} placeholder="Enter Name" />
+        <input type="text" value={state.email} placeholder="Email" />
+        <input type="text" value={state.address} placeholder="Adderss" />
+        <input type="text" value={state.phone} placeholder="phone" />
+        <button>Submit</button>
+      </div>
     </div>
   )
 }
