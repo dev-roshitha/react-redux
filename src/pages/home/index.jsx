855dcf3ae99354d03ddaeb86db37d668e3596522
unknown <sajithroshitha98@gmail.com>
2023-09-13 00:01:53 +0530

    update app

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,25 +1,49 @@
 import { useReducer } from "react";
 
-const reducer = (state, action)=> {}
+const reducer = (state, action)=> {
+  if(action.type === 'setName'){
+    return {...state, name: action.data}
+  }else if(action.type === 'setEmail'){
+    return {...state, email: action.data}
+  }else if(action.type === 'setAddress'){
+    return {...state, address: action.address}
+  }else if(action.type === 'setPhone'){
+    return {...state, phone: action.address}
+  }else{
+    return state
+  }
+}
 
 const Home = () => {
-
+  
   const [state, setState] = useReducer(reducer, {
-    name: "roshitha",
-    email: "r@gmail.com",
-    address: "34/18 Kandy Srilanka",
-    phone: "0703639744"
+    name: '',
+    email: '',
+    address: '',
+    phone: '',
   })
-
+  console.log(state)
   return(
     <div>
       <h1>Home Page WITH REACT ROUTER DOM</h1>
       <section className="wrapper">
         <div className="home-wrapper">
-          <input type="text" value={state.name} placeholder="Enter Name" />
-          <input type="text" value={state.email} placeholder="Email" />
-          <input type="text" value={state.address} placeholder="Adderss" />
-          <input type="text" value={state.phone} placeholder="phone" />
+          <input type="text" value={state.name} onChange={(e)=> setState({
+            type: 'setName',
+            data: e.target.value
+          })} placeholder="Enter Name" />
+          <input type="text" value={state.email} onChange={(e) => setState({
+            type: 'setEmail',
+            data: e.target.value
+          })} placeholder="Email" />
+          <input type="text" value={state.address} onChange={(e) => setState({
+            type: 'setAddress',
+            data: e.target.value
+          })} placeholder="Adderss" />
+          <input type="text" value={state.phone} onChange={(e) => setState({
+            type: 'setPhone',
+            data: e.target.value
+          })} placeholder="phone" />
           <button>Submit</button>
         </div>
       </section>
