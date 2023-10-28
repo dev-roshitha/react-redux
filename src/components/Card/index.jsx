0e1f765de2a4f23516c60e555bcb38850fc44d8e
unknown <sajithroshitha98@gmail.com>
2023-10-28 22:07:25 +0530

    update conditional redering

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,22 @@
 import PropTypes from "prop-types"
+import React, {useState} from "react"
 
 const Card = (props) => {
+
+  const [loged, setLoged] = useState(false);
+
+  const handleLog = () => {
+    setLoged(!loged)
+  }
+
   return(
   <div className="card">
     <h2>{props.name}</h2>
     <h2>{props.job}</h2>
     <h5>{props.age}</h5>
+    <br/>
+    <h1>{loged ? 'User Loged-in' : 'User Loged-out'}</h1>
+    <button onClick={handleLog}>{loged ? 'Logout' : 'Login'}</button>
   </div>
   )
 }
